a0cf222fa2fc2208d2ded90b0e1c3118251b2b85
Amiya <amiyanayak3105@gmail.com>
2024-08-14 10:50:20 +0530

    change the file name

--- RockPaperScissor.java
+++ RockPaperScissor.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class RockPaperScissors {
+public class RockPaperScissor {
     public static void main(String[] args) {
         
         Scanner scan = new Scanner(System.in);
